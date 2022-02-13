e74c1adaa94c946fb80da0d47c476e263d258efb
Ray Zilliox <ray@kangarootime.com>
2022-02-13 17:30:21 -0500

    playing with the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+const TopicsPage = () => (
+  <div>
+    <h1>Topics Page</h1>
+  </div>
+);
+
+const TopicsDetailPage = (props) => (
+  <div>
+    <h1>Topics Detail Page: {props.match.params.topicId}</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/topics' component={TopicsPage} />
+        <Route path='/topics/:topicId' component={TopicsDetailPage} />
+      </Switch>
+      {/* <HomePage></HomePage> */}
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Directory from "../../components/menu-item/directory/directory.component";
 import './homepage.styles.scss';
+import { Link } from "react-router-dom";
 
-const HomePage = () => (
+const HomePage = props => (
     <div className='homepage'>
+        <Link to='/topics'>Topics</Link>
+        <button onClick={() => props.history.push('/topics')}>Topics</button>
         <Directory></Directory>
     </div>
 );
