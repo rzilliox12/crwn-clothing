67860a16d506496cd552b15ec3d7603111d9b639
Ray Zilliox <ray@kangarootime.com>
2022-02-20 12:05:37 -0500

    added withRouter and links to each section

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,8 @@ import Directory from "../../components/menu-item/directory/directory.component"
 import './homepage.styles.scss';
 import { Link } from "react-router-dom";
 
-const HomePage = props => (
+const HomePage = () => (
     <div className='homepage'>
-        <Link to='/topics'>Topics</Link>
-        <button onClick={() => props.history.push('/topics')}>Topics</button>
         <Directory></Directory>
     </div>
 );
